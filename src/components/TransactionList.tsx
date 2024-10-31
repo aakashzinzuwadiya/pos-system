ca81e72f634694e88cc9ba20e0e56375f5a60f13
AakashZinzuwadiya <aakashsoni96@gmail.com>
2024-10-31 22:37:28 +0000

    Update TransactionList.tsx

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -47,6 +47,7 @@ const TransactionList: React.FC<TransactionListProps> = ({
         Item: item.name,
         ItemPrice: item.price.toFixed(2),
         TotalAmount: transaction.totalAmount.toFixed(2),
+        PaymentMethod: transaction.PaymentMethod,
         UserEmail: transaction.email
       }))
     );    
